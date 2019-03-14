c2d4da32108c0be56c540ea564d55e08958ab778
Balvinder Singh <balvinder.singh@nhs.net>
2019-03-14 09:29:25 +0000

    2019-03-14 Mostly working

--- HSABMCAWeb/Default.aspx
+++ HSABMCAWeb/Default.aspx
@@ -1,42 +1,62 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="HSABMCAWeb._Default" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-
-    <div class="jumbotron">
-        <h1>ASP.NET</h1>
-        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
-        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn morelll &raquo;</a></p>
+    <asp:Image ID="Image1" runat="server" ImageUrl="~/Images/people-300x79.png" />
+    <div >
+        <h2>
+            <asp:Table runat="server" id="tbl01_Head">
+                <asp:TableRow runat="server" ID="tr01_MCD"><asp:TableCell>
+                        <asp:Button ID="btn01_MCD" runat="server" Text="Mental Capacity Decision Prompts"  Width="500px" OnClick="btn01_Click" />
+                 </asp:TableCell><asp:TableCell>
+                        <asp:Button ID="btn01_InfoMCD" runat="server" Width="58px"  OnClick="btn01_Click" Text="i" Font-Italic="True" />
+                </asp:TableCell></asp:TableRow>
+                <asp:TableRow runat="server" ID="tr01_BID"><asp:TableCell>
+                        <asp:Button ID="btn01_BID" runat="server" Text="Best Interests Decision Prompts" Width="500px" />
+                </asp:TableCell><asp:TableCell>
+                        <asp:ImageButton ID="btn01_InfoBID" runat="server" ImageUrl="~/Images/info-blue.png" Height="34px" Width="58px" />
+                </asp:TableCell></asp:TableRow>
+                <asp:TableRow runat="server" ID="tr01_IMCA"><asp:TableCell>
+                        <asp:Button ID="btn01_IMCA" runat="server" Text="IMCA Referral Prompts" Width="500px" /><br />
+                </asp:TableCell><asp:TableCell>
+                        <asp:ImageButton ID="btn01_InfoIMCA" runat="server" ImageUrl="~/Images/info-blue.png" Height="34px" Width="58px" />
+                </asp:TableCell></asp:TableRow>
+                <asp:TableRow runat="server" ID="tr01_DOLS"><asp:TableCell>
+                        <asp:Button ID="btn01_DOLS" runat="server" Text="DOLS Application Prompts" Width="500px" /><br />
+                </asp:TableCell><asp:TableCell>
+                        <asp:ImageButton ID="btn01_InfoDOLS" runat="server" ImageUrl="~/Images/info-blue.png" Height="34px" Width="58px" />
+                </asp:TableCell></asp:TableRow></asp:Table>
+</h2>
     </div>
+    <asp:Panel runat="server" ID="pnl02_Page">
+        <h2>
+        <asp:Label ID="lbl02_Prompt" runat="server" Text="Label"></asp:Label></h2>
+        <asp:Panel runat="server" ID="pnl02A_Prompt">
+           <br /> <div style="margin-left: 100px">
+                <h2>
+                    <asp:Table ID="tbl02_Prompt" runat="server">
+                        <asp:TableRow runat="server" ID="tr02_Yes">
+                            <asp:TableCell>
+                                <asp:ImageButton ID="img02_Yes" runat="server" ImageUrl="~/Images/BigTick.png" Height="29px" Width="34px" OnClick="img02_Yes_Click" />
+                            </asp:TableCell>
+                            <asp:TableCell>Yes</asp:TableCell>
+                        </asp:TableRow>
+                        <asp:TableRow runat="server" ID="tr02_No">
+                            <asp:TableCell>
+                                <asp:ImageButton ID="img02_No" runat="server" ImageUrl="~/Images/BigCross.png" Height="29px" Width="31px" OnClick="img02_No_Click" />
+                            </asp:TableCell>
+                            <asp:TableCell>No
+                            </asp:TableCell>
+                        </asp:TableRow>
+                    </asp:Table>
+                </h2>
+            </div>
+        </asp:Panel>
+    </asp:Panel>
 
-    <div class="row">
-        <div class="col-md-4">
-            <h2>Getting started</h2>
-            <p>
-                ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
-            A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Get more libraries</h2>
-            <p>
-                NuGet is a free Visual Studio extension that makes it easy to add, remove, and update libraries and tools in Visual Studio projects.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301949">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Web Hosting</h2>
-            <p>
-                You can easily find a web hosting company that offers the right mix of features and price for your applications.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
-            </p>
-        </div>
-    </div>
-
+    <asp:Panel runat="server" ID="pnl99_Footer">
+        <br />
+    <asp:ImageButton ID="img99_Back" runat="server" ImageUrl="~/Images/back.png" Height="34px" Width="34px"  OnClick="img99_Back_Click" />
+    <asp:ImageButton ID="img99_Next" runat="server" ImageUrl="~/Images/next.png" Height="34px" Width="34px"  OnClick="img99_Next_Click"/>
+    <asp:ImageButton ID="img99_Home" runat="server" ImageUrl="~/Images/home.png" Height="34px" Width="34px" OnClick="img99_Home_Click"/>
+    </asp:Panel>
 </asp:Content>
--- HSABMCAWeb/Default.aspx.cs
+++ HSABMCAWeb/Default.aspx.cs
@@ -4,14 +4,198 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.Sql;
 
 namespace HSABMCAWeb
 {
     public partial class _Default : Page
     {
+        dsWebTableAdapters.PagesTableAdapter tadPages = new dsWebTableAdapters.PagesTableAdapter();
+        dsWeb.PagesDataTable tblPages = new dsWeb.PagesDataTable();
+
+        Button btn;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (IsPostBack)
+            {
+                return;
+            }
+            if (Request.QueryString["PageID"] != null)
+            {
+                tr01_MCD.Visible = false;
+                tr01_BID.Visible = false;
+                tr01_IMCA.Visible = false;
+                tr01_DOLS.Visible = false;
+                pnl99_Footer.Visible = true;
+                string page_id = Request.QueryString["PageID"].ToString().ToLower();
+                tadPages.Fill(tblPages, Page_ID: page_id);
+                page_id = "home";
+                foreach (dsWeb.PagesRow prow in tblPages)
+                {
+                    page_id = prow.Page_ID;
+                    switch (prow.HeaderType)
+                    {
+                        case "MCD":
+                            tr01_MCD.Visible = true;
+                            break;
+                        case "BID":
+                            tr01_BID.Visible = true;
+                            break;
+                        case "IMCA":
+                            tr01_IMCA.Visible = true;
+                            break;
+                        case "DOLS":
+                            tr01_DOLS.Visible = true;
+                            break;
+                        default:
+                            page_id = "home";
+                            break;
+                    }
+                }
+                Load_Page(page_id: page_id);
+                return;
+            }
+            pnl99_Footer.Visible = false;
+            img99_Next.Visible = false;
+            pnl02_Page.Visible = false;
+        }
+
+        protected void btn01_Click(object sender, EventArgs e)
+        {
+            btn = (Button)sender;
+            if (!btn.ToolTip.Equals(""))
+            {
+                Load_Page(page_id: btn.ToolTip);
+                return;
+            }
+
+            tr01_MCD.Visible = false;
+            tr01_BID.Visible = false;
+            tr01_IMCA.Visible = false;
+            tr01_DOLS.Visible = false;
+            pnl99_Footer.Visible = true;
+
+            switch (btn.ID)
+            {
+                case "btn01_MCD":
+                    tr01_MCD.Visible = true;
+                    btn.ToolTip = "Slide01";
+                    Load_Page(page_id: btn.ToolTip);
+                    pnl02_Page.Visible = true;
+                    break;
+                case "btn01_InfoMCD":
+                    tr01_MCD.Visible = true;
+                    break;
+            }
 
         }
+        protected void img02_Yes_Click(object sender, ImageClickEventArgs e)
+        {
+            Load_Page(page_id: img02_Yes.ToolTip);
+        }
+
+        protected void img02_No_Click(object sender, ImageClickEventArgs e)
+        {
+            Load_Page(page_id: img02_No.ToolTip);
+        }
+
+        protected void Load_Page(string page_id)
+        {
+            if (page_id.ToLower().Equals("home"))
+            {
+                Load_Home();
+                return;
+            }
+            tadPages.Fill(tblPages, Page_ID: page_id);
+            foreach(dsWeb.PagesRow prow in tblPages)
+            {
+                pnl02_Page.Visible = true;
+                lbl02_Prompt.Text = prow.DisplayText;
+                if (prow.IsYesPage_IDNull())
+                {
+                    tr02_Yes.Visible = false;
+                    img02_Yes.ToolTip = "Home";
+                }
+                else
+                {
+                    tr02_Yes.Visible = true;
+                    img02_Yes.ToolTip = prow.YesPage_ID;
+
+                }
+                if (prow.IsNoPage_IDNull())
+                {
+                    tr02_No.Visible = false;
+                    img02_No.ToolTip = "Home";
+                }
+                else
+                {
+                    tr02_No.Visible = true;
+                    img02_No.ToolTip = prow.NoPage_ID;
+                }
+                pnl02A_Prompt.Visible = tr02_Yes.Visible || tr02_No.Visible;
+                if (prow.IsBackPage_IDNull())
+                {
+                    img99_Back.Visible = false;
+                    img99_Back.ToolTip = "Home";
+                }
+                else
+                {
+                    img99_Back.Visible = true;
+                    img99_Back.ToolTip = prow.BackPage_ID;
+                }
+                if (prow.IsNextPage_IDNull())
+                {
+                    img99_Next.Visible = false;
+                    img99_Next.ToolTip = "Home";
+                }
+                else
+                {
+                    img99_Next.Visible = true;
+                    img99_Next.ToolTip = prow.NextPage_ID;
+                }
+                return;
+            }
+            img99_Back.Visible = false;
+            img99_Next.Visible = false;
+            tr02_Yes.Visible = false;
+            tr02_No.Visible = false;
+            lbl02_Prompt.Text = page_id+ " page not found - please report to support ";
+        }
+
+        #region pnl99_Footer
+
+        protected void img99_Home_Click(object sender, ImageClickEventArgs e)
+        {
+            Load_Home();
+        }
+
+        protected void Load_Home()
+        { 
+            tr01_MCD.Visible = true;
+            tr01_BID.Visible = true;
+            tr01_IMCA.Visible = true;
+            tr01_DOLS.Visible = true;
+            pnl99_Footer.Visible = false;
+            img99_Next.Visible = false;
+            pnl02_Page.Visible = false;
+
+        }
+
+        protected void img99_Next_Click(object sender, ImageClickEventArgs e)
+        {
+            Load_Page(page_id: img99_Next.ToolTip);
+        }
+
+
+        #endregion
+
+        protected void img99_Back_Click(object sender, ImageClickEventArgs e)
+        {
+            Load_Page(page_id: img99_Back.ToolTip);
+
+        }
+
+
     }
 }
